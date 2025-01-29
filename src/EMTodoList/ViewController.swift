daac1f26578ec1ac0e5e64110b9266e51eeb23a2
Mikhail Bereberdin <bereberdin999@icloud.com>
2025-01-29 17:20:40 +0700

    Инициализация проекта

---
+++ src/EMTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMTodoList
+//
+//  Created by Mikhail Bereberdin on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
